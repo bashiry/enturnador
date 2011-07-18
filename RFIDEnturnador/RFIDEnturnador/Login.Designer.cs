0ecd298c5e00f0c8920ab40826426da10fc6b2f0
mariopena77 <mariopena77@6798bb9d-fe6e-06b3-8d18-2b815bfc2219>
2011-07-18 03:06:25 +0000

    Inicio de UI del login

---
+++ RFIDEnturnador/RFIDEnturnador/Login.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace RFIDEnturnador
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Cancel = new System.Windows.Forms.Button();
+            this.OK = new System.Windows.Forms.Button();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.UsernameLabel = new System.Windows.Forms.Label();
+            this.LogoPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Cancel
+            // 
+            this.Cancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.Cancel.Image = global::RFIDEnturnador.Properties.Resources.cancelar;
+            this.Cancel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.Cancel.Location = new System.Drawing.Point(257, 133);
+            this.Cancel.Name = "Cancel";
+            this.Cancel.Size = new System.Drawing.Size(86, 24);
+            this.Cancel.TabIndex = 12;
+            this.Cancel.Text = "   &Cancelar";
+            this.Cancel.Click += new System.EventHandler(this.Cancel_Click);
+            // 
+            // OK
+            // 
+            this.OK.Image = global::RFIDEnturnador.Properties.Resources.chulo;
+            this.OK.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.OK.Location = new System.Drawing.Point(165, 133);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(86, 24);
+            this.OK.TabIndex = 11;
+            this.OK.Text = "   &Ingresar";
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(164, 89);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.PasswordChar = '*';
+            this.txtClave.Size = new System.Drawing.Size(179, 20);
+            this.txtClave.TabIndex = 10;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(164, 32);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(179, 20);
+            this.txtUsuario.TabIndex = 8;
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.Location = new System.Drawing.Point(162, 69);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(220, 23);
+            this.PasswordLabel.TabIndex = 9;
+            this.PasswordLabel.Text = "Clave";
+            this.PasswordLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // UsernameLabel
+            // 
+            this.UsernameLabel.Location = new System.Drawing.Point(162, 12);
+            this.UsernameLabel.Name = "UsernameLabel";
+            this.UsernameLabel.Size = new System.Drawing.Size(220, 23);
+            this.UsernameLabel.TabIndex = 6;
+            this.UsernameLabel.Text = "Usuario";
+            this.UsernameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // LogoPictureBox
+            // 
+            this.LogoPictureBox.Image = global::RFIDEnturnador.Properties.Resources._lock;
+            this.LogoPictureBox.Location = new System.Drawing.Point(12, 12);
+            this.LogoPictureBox.Name = "LogoPictureBox";
+            this.LogoPictureBox.Size = new System.Drawing.Size(131, 145);
+            this.LogoPictureBox.TabIndex = 7;
+            this.LogoPictureBox.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AcceptButton = this.OK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 186);
+            this.Controls.Add(this.Cancel);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.UsernameLabel);
+            this.Controls.Add(this.LogoPictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button Cancel;
+        internal System.Windows.Forms.Button OK;
+        internal System.Windows.Forms.TextBox txtClave;
+        internal System.Windows.Forms.TextBox txtUsuario;
+        internal System.Windows.Forms.Label PasswordLabel;
+        internal System.Windows.Forms.Label UsernameLabel;
+        internal System.Windows.Forms.PictureBox LogoPictureBox;
+    }
+}
---
+++ RFIDEnturnador/RFIDEnturnador/Login.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace RFIDEnturnador
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            if (this.Validar())
+            {
+                try
+                {
+                    Cursor = Cursors.WaitCursor;
+
+                    Thread th1 = new Thread(new ThreadStart(Run));
+                    th1.Start();
+
+                    Cursor = Cursors.Default;
+                    this.Close();                    
+                }
+                catch (Exception ex)
+                {
+                    Cursor = Cursors.Default;
+                    MessageBox.Show(ex.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);    
+                }            
+            }
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private bool Validar()
+        {
+            string mensaje = "";
+
+            if (this.txtUsuario.Text.Trim().Length == 0)
+                mensaje += "- Debe escribir el usuario\n";
+
+            if (this.txtClave.Text.Trim().Length == 0)
+                mensaje += "- Debe escribir la clave\n";
+
+            if (mensaje.Length > 0)
+            {
+                MessageBox.Show(mensaje, "Datos incompletos", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return false;
+            }
+
+            return true;        
+        }
+
+        private void Run()
+        { 
+            Application.Run(new Principal());
+        }
+
+
+    }
+}
