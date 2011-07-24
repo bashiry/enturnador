0a3105a8e6aabd95665ea276336a79a78fd3e4cd
mariopena77 <mariopena77@6798bb9d-fe6e-06b3-8d18-2b815bfc2219>
2011-07-24 18:08:16 +0000

    Inicio monitor

--- trunk/RFIDEnturnador/RFIDEnturnador/Monitor.Designer.cs
+++ trunk/RFIDEnturnador/RFIDEnturnador/Monitor.Designer.cs
@@ -29,10 +29,292 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle25 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle26 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle27 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle28 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.lblTipoCargue = new System.Windows.Forms.Label();
+            this.lblHoraActual = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.grd1 = new System.Windows.Forms.DataGridView();
+            this.NO = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PLACA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.HORA = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.grd2 = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.txtNoticias = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grd1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grd2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(882, 119);
+            this.panel2.TabIndex = 7;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RFIDEnturnador.Properties.Resources.camionPequeno;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 5);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label1.Location = new System.Drawing.Point(232, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(274, 35);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Turnos de entrada";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::RFIDEnturnador.Properties.Resources.camionPequeno;
+            this.pictureBox2.Location = new System.Drawing.Point(118, 5);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // lblTipoCargue
+            // 
+            this.lblTipoCargue.AutoSize = true;
+            this.lblTipoCargue.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipoCargue.Location = new System.Drawing.Point(21, 23);
+            this.lblTipoCargue.Name = "lblTipoCargue";
+            this.lblTipoCargue.Size = new System.Drawing.Size(228, 32);
+            this.lblTipoCargue.TabIndex = 8;
+            this.lblTipoCargue.Text = "Tipo de Cargue:";
+            // 
+            // lblHoraActual
+            // 
+            this.lblHoraActual.AutoSize = true;
+            this.lblHoraActual.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHoraActual.Location = new System.Drawing.Point(444, 23);
+            this.lblHoraActual.Name = "lblHoraActual";
+            this.lblHoraActual.Size = new System.Drawing.Size(195, 32);
+            this.lblHoraActual.TabIndex = 9;
+            this.lblHoraActual.Text = "Fecha y hora:";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.txtNoticias);
+            this.panel1.Controls.Add(this.grd2);
+            this.panel1.Controls.Add(this.grd1);
+            this.panel1.Controls.Add(this.lblTipoCargue);
+            this.panel1.Controls.Add(this.lblHoraActual);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 119);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(882, 536);
+            this.panel1.TabIndex = 8;
+            // 
+            // grd1
+            // 
+            this.grd1.AllowUserToAddRows = false;
+            this.grd1.AllowUserToDeleteRows = false;
+            this.grd1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grd1.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            dataGridViewCellStyle25.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle25.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle25.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle25.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle25.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle25.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle25.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grd1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle25;
+            this.grd1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grd1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.NO,
+            this.PLACA,
+            this.HORA});
+            this.grd1.Location = new System.Drawing.Point(27, 72);
+            this.grd1.Margin = new System.Windows.Forms.Padding(4);
+            this.grd1.Name = "grd1";
+            this.grd1.ReadOnly = true;
+            dataGridViewCellStyle26.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle26.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle26.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle26.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle26.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle26.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle26.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grd1.RowHeadersDefaultCellStyle = dataGridViewCellStyle26;
+            this.grd1.RowHeadersVisible = false;
+            this.grd1.RowHeadersWidth = 20;
+            this.grd1.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.grd1.RowTemplate.Height = 24;
+            this.grd1.Size = new System.Drawing.Size(400, 343);
+            this.grd1.TabIndex = 18;
+            // 
+            // NO
+            // 
+            this.NO.DataPropertyName = "no";
+            this.NO.HeaderText = "No.";
+            this.NO.Name = "NO";
+            this.NO.ReadOnly = true;
+            // 
+            // PLACA
+            // 
+            this.PLACA.DataPropertyName = "placa";
+            this.PLACA.FillWeight = 45.16006F;
+            this.PLACA.HeaderText = "PLACA";
+            this.PLACA.Name = "PLACA";
+            this.PLACA.ReadOnly = true;
+            // 
+            // HORA
+            // 
+            this.HORA.DataPropertyName = "hora";
+            this.HORA.HeaderText = "HORA";
+            this.HORA.Name = "HORA";
+            this.HORA.ReadOnly = true;
+            // 
+            // grd2
+            // 
+            this.grd2.AllowUserToAddRows = false;
+            this.grd2.AllowUserToDeleteRows = false;
+            this.grd2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.grd2.BackgroundColor = System.Drawing.SystemColors.ControlLight;
+            dataGridViewCellStyle27.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle27.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle27.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle27.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle27.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle27.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle27.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grd2.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle27;
+            this.grd2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grd2.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3});
+            this.grd2.Location = new System.Drawing.Point(450, 72);
+            this.grd2.Margin = new System.Windows.Forms.Padding(4);
+            this.grd2.Name = "grd2";
+            this.grd2.ReadOnly = true;
+            dataGridViewCellStyle28.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle28.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle28.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle28.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle28.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle28.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle28.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.grd2.RowHeadersDefaultCellStyle = dataGridViewCellStyle28;
+            this.grd2.RowHeadersVisible = false;
+            this.grd2.RowHeadersWidth = 20;
+            this.grd2.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.PaleGoldenrod;
+            this.grd2.RowTemplate.Height = 24;
+            this.grd2.Size = new System.Drawing.Size(400, 343);
+            this.grd2.TabIndex = 19;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "no";
+            this.dataGridViewTextBoxColumn1.HeaderText = "No.";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            this.dataGridViewTextBoxColumn1.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "placa";
+            this.dataGridViewTextBoxColumn2.FillWeight = 45.16006F;
+            this.dataGridViewTextBoxColumn2.HeaderText = "PLACA";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            this.dataGridViewTextBoxColumn2.ReadOnly = true;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "hora";
+            this.dataGridViewTextBoxColumn3.HeaderText = "HORA";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            this.dataGridViewTextBoxColumn3.ReadOnly = true;
+            // 
+            // txtNoticias
+            // 
+            this.txtNoticias.Font = new System.Drawing.Font("Arial", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNoticias.ForeColor = System.Drawing.Color.OrangeRed;
+            this.txtNoticias.Location = new System.Drawing.Point(27, 422);
+            this.txtNoticias.Name = "txtNoticias";
+            this.txtNoticias.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtNoticias.Size = new System.Drawing.Size(823, 61);
+            this.txtNoticias.TabIndex = 20;
+            this.txtNoticias.Text = "PRUEBA";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Monitor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Monitor";
+            this.ClientSize = new System.Drawing.Size(882, 655);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Name = "Monitor";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Control de ingreso";
+            this.Load += new System.EventHandler(this.Monitor_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.grd1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.grd2)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label lblTipoCargue;
+        private System.Windows.Forms.Label lblHoraActual;
+        private System.Windows.Forms.Panel panel1;
+        internal System.Windows.Forms.DataGridView grd2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        internal System.Windows.Forms.DataGridView grd1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NO;
+        private System.Windows.Forms.DataGridViewTextBoxColumn PLACA;
+        private System.Windows.Forms.DataGridViewTextBoxColumn HORA;
+        private System.Windows.Forms.TextBox txtNoticias;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- trunk/RFIDEnturnador/RFIDEnturnador/Monitor.cs
+++ trunk/RFIDEnturnador/RFIDEnturnador/Monitor.cs
@@ -6,14 +6,74 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using EnturnadorDAO;
+using EnturnadorLIB;
+
 
 namespace RFIDEnturnador
 {
     public partial class Monitor : Form
     {
+        private EnturnadorLIB.Enturnador.Noticia objNoticia;
+        private string noticias;
+        private int cantidadCaracteres;
+        private int contadorCaracteres;
+        private const int CARACTERES_VISIBLES_NOTICIA = 50;
+
         public Monitor()
         {
             InitializeComponent();
         }
+
+        private void Monitor_Load(object sender, EventArgs e)
+        {
+            this.objNoticia = new EnturnadorLIB.Enturnador.Noticia();
+            this.GetNoticias();
+            this.timer1.Enabled = true;
+        }
+
+        #region "Métodos privados"
+
+        /// <summary>
+        /// Obtiene las noticias activas en el momento
+        /// </summary>
+        private void GetNoticias()
+        {
+            List<NOTICIA> listaNoticias = this.objNoticia.GetAll();
+
+            //Construye el string con las noticias
+            this.noticias = "";
+            foreach (NOTICIA noti in listaNoticias)
+            {
+                noticias += noti.noticia1 + " - ";
+            }
+
+            //Se quita el ultimo guion
+            if (this.noticias.Length > 0)
+                this.noticias = this.noticias.Substring(0, this.noticias.Length - 2);
+
+            //Suma algunos caracteres de mas para que alcance a salir la última noticia completa
+            this.cantidadCaracteres = this.noticias.Length + 15;
+            this.contadorCaracteres = 0;
+        }
+
+        #endregion
+
+        #region "Eventos"
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Si ya se recorrieron todas la noticias se reinicia
+            if (this.contadorCaracteres >= this.cantidadCaracteres)
+                this.GetNoticias();
+
+            //Mueve el texto que sale en las noticias, muestra siempre la misma cantidad de caracteres
+            string texto = this.noticias.Substring(this.contadorCaracteres, CARACTERES_VISIBLES_NOTICIA);
+            this.contadorCaracteres += 1;
+
+            this.txtNoticias.Text = texto;
+        }
+
+        #endregion
     }
 }
