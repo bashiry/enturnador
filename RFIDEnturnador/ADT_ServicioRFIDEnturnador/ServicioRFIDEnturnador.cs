a5d693dd21d8da2a29494c680f2f8edda408e863
mariopena77 <mariopena77@6798bb9d-fe6e-06b3-8d18-2b815bfc2219>
2011-08-29 15:17:38 +0000

    Servicio Windows para realizar las lecturas RFID (ya no va en la aplicacion de escritorio)

---
+++ RFIDEnturnador/ADT_ServicioRFIDEnturnador/ServicioRFIDEnturnador.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace ADT_ServicioRFIDEnturnador
+{
+    partial class ServicioRFIDEnturnador : ServiceBase
+    {
+        LLRPReadControl ctrl = new LLRPReadControl();
+        private const string LLAVE_PUERTA_E1 = "ID_PUERTA_E1";  //Llave que se busca en la base de datos apra obtener el id de la puerta que enturna
+        private const string LLAVE_PUERTA_E2 = "ID_PUERTA_E2";  //Llave que se busca en la base de datos apra obtener el id de la puerta que desenturna        
+
+        public ServicioRFIDEnturnador()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            this.IniciarLectura();
+        }
+
+        protected override void OnStop()
+        {
+            //Se detienen las lecturas
+            this.ctrl.Detener();
+        }
+
+        private void IniciarLectura()
+        {
+            //Se obtienen los ids de las puertas que enturnan y desenturnan
+            EnturnadorLIB.Enturnador.Configuracion objConfig = new EnturnadorLIB.Enturnador.Configuracion();
+            CGlobal.ID_PUERTA_E1 = Convert.ToInt32(objConfig.GetValorConfig(LLAVE_PUERTA_E1));
+            CGlobal.ID_PUERTA_E2 = Convert.ToInt32(objConfig.GetValorConfig(LLAVE_PUERTA_E2));
+
+            string rutaConfig = ADT_ServicioRFIDEnturnador.Properties.Settings.Default.PathDevicesXml;
+            this.ctrl.Iniciar(ADT_ServicioRFIDEnturnador.Properties.Settings.Default.TiempoVerificacion, rutaConfig);
+        }
+
+    }
+}
